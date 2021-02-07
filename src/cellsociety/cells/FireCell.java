e4ea53a70f7bc893b3350406ad71a7263b891e3d
Livia <las120@duke.edu>
2021-02-07 17:22:20 -0500

    created basic fire cell class

--- src/cellsociety/cells/FireCell.java
+++ src/cellsociety/cells/FireCell.java
@@ -1,2 +1,27 @@
-package cellsociety.cells;public class FireCell {
+package cellsociety.cells;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Cell class for fire simulation cells.
+ *
+ * @author Livia Seibert
+ */
+public class FireCell extends Cell {
+
+  private List<Cell> neighbors;
+
+  public FireCell() {
+    neighbors = new ArrayList<Cell>();
+  }
+
+  /**
+   * Accesses the adjacent cells needed to update the state of the cell.
+   *
+   * @return list of neighboring cells
+   */
+  public List<Cell> getNeighbors() {
+    return neighbors;
+  }
 }
--- src/cellsociety/grid/Grid.java
+++ src/cellsociety/grid/Grid.java
@@ -2,6 +2,11 @@ package cellsociety.grid;
 
 import cellsociety.cells.Cell;
 
+/**
+ * Class that holds cell in proper formation for simulation.
+ *
+ * @author Livia Seibert
+ */
 public class Grid {
 
   Cell[][] grid;
@@ -10,6 +15,13 @@ public class Grid {
     grid = new Cell[width][height];
   }
 
+  /**
+   * Allows access to cell objects at specific locations.
+   *
+   * @param i row of cell
+   * @param j col of cell
+   * @return Cell object located at indicated position in grid
+   */
   public Cell getCellAtLocation(int i, int j) {
     if (i >= 0 && i < grid.length && j >= 0 && j < grid[i].length) {
       return grid[i][j];
