862a4d76950056378c4617f677eabea1cb850452
Billy Luqiu <wyl6@duke.edu>
2021-02-08 00:51:23 -0500

    added method to getNewGrid to Controller class

--- src/cellsociety/Controller.java
+++ src/cellsociety/Controller.java
@@ -2,8 +2,13 @@ package cellsociety;
 
 public abstract class Controller {
 
+  public Grid oldGrid;
+  public Grid newGrid;
   public abstract void updateState();
   public abstract boolean simulationEnded();
 
+  public Grid getNewGrid()  {
+    return newGrid;
+  }
 
 }
