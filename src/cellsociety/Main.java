a065464d942d9b761ea98ebbc9d602f268fa0bfe
Livia <las120@duke.edu>
2021-02-08 14:27:11 -0500

    fixing merge conflicts

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -1,5 +1,7 @@
 package cellsociety;
 
+import cellsociety.grid.XMLParser;
+
 /**
  * Feel free to completely change this code or delete it entirely. 
  */
@@ -8,6 +10,11 @@ public class Main {
      * Start of the program.
      */
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        XMLParser parser = new XMLParser("firestandard.xml");
+        try {
+            parser.readFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
