440873848f4d8b3b92305d69f2fda9a84b667dd7
Billy Luqiu <wyl6@duke.edu>
2021-02-07 23:35:29 -0500

    added basic fire controller class

---
+++ src/cellsociety/FireController.java
@@ -0,0 +1,14 @@
+package cellsociety;
+
+public class FireController extends Controller{
+
+  @Override
+  public void updateState() {
+
+  }
+
+  @Override
+  public boolean simulationEnded() {
+    return false;
+  }
+}
