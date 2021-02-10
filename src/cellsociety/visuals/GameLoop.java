03e9c985d236ad94faa1e46b886e0ff36381609f
Carola Maglione <cm524@duke.edu>
2021-02-10 15:24:41 -0500

    stage is showing and working

--- src/cellsociety/visuals/GameLoop.java
+++ src/cellsociety/visuals/GameLoop.java
@@ -3,14 +3,35 @@ package cellsociety.visuals;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 public class GameLoop extends Application {
     private Graphics visuals = new Graphics();
 
+    private Scene myScene;
+    private static final int HEIGHT = 800;
+    private static final int WIDTH = 800;
+    public static final Paint BACKGROUND = Color.AZURE;
+    private static final String TITLE = "Cellular Automata";
+
     private void step(double elapsedTime){
+    }
 
+    private Scene creatingLandingScreen(int width, int height, Paint background){
+        Group root = new Group();
+        Text welcome = new Text(75, 100, "Simulation Menu");
+        Text instructions = new Text(75, 150, "click on any simulation to start");
 
+        root.getChildren().add(welcome);
+        root.getChildren().add(instructions);
+
+        Scene scene = new Scene(root, width, height, background);
+
+        return scene;
     }
 
     private void updateGameState(){
@@ -26,10 +47,9 @@ public class GameLoop extends Application {
 //    }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        Group root = visuals.createLandingScreen();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+    public void start(Stage stage){
+        myScene = creatingLandingScreen(WIDTH, HEIGHT, BACKGROUND);
+        stage.setScene(myScene);
         stage.show();
     }
 }
--- src/cellsociety/visuals/Graphics.java
+++ src/cellsociety/visuals/Graphics.java
@@ -2,26 +2,29 @@ package cellsociety.visuals;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Graphics {
 
     Group root;
-    public static int height = 800;
-    public static int width = 800;
     public static String font = "Verdana";
+
+
+
+    public static final Paint HIGHLIGHT = Color.OLIVEDRAB;
 
 //    public Group getCurrentDisplay(Grid grid){
 //
 //    }
 
-    public Group createLandingScreen(){
-        Text welcome = new Text(75, 100, "Simulation Menu");
-        root.getChildren().add(welcome);
-        Text instructions = new Text(75, 150, "click on any simulation to start");
-
-        return root;
-
-    }
+//    public void createLandingScreen(Group root, int ){
+//        Text welcome = new Text(75, 100, "Simulation Menu");
+//        root.getChildren().add(welcome);
+//        Text instructions = new Text(75, 150, "click on any simulation to start");
+//
+//
+//    }
 }
