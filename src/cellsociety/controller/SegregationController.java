f4455d7bf6b47f15d357c2a534fd05f2fa445e26
Billy Luqiu <wyl6@duke.edu>
2021-02-10 03:00:28 -0500

    implemented getIsSatisfied method

--- src/cellsociety/controller/SegregationController.java
+++ src/cellsociety/controller/SegregationController.java
@@ -1,5 +1,6 @@
 package cellsociety.controller;
 
+import cellsociety.cells.SegregationCell;
 import cellsociety.grid.Grid;
 
 public class SegregationController extends Controller{
@@ -11,6 +12,19 @@ public class SegregationController extends Controller{
 
   @Override
   public boolean simulationEnded() {
-    return false;
+    Grid oldGrid = super.getOldGrid();
+    Grid newGrid = super.getNewGrid();
+    int[] dims = oldGrid.getSizeOfGrid();
+
+    for (int i = 0; i < dims[0]; i++) {
+      for (int j = 0; j< dims[1]; j++)  {
+        SegregationCell oldCell = (SegregationCell) oldGrid.getCellAtLocation(i, j);
+        SegregationCell newCell = (SegregationCell) newGrid.getCellAtLocation(i, j);
+        if (oldCell.getIsSatisfied() != newCell.getIsSatisfied()) {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
