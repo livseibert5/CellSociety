6a42205e0fd04f436f3efd9598ff85f8d5001384
Billy Luqiu <wyl6@duke.edu>
2021-02-10 02:39:57 -0500

    implemented simulationEnded for fireController

--- src/cellsociety/controller/FireController.java
+++ src/cellsociety/controller/FireController.java
@@ -1,6 +1,7 @@
 package cellsociety.controller;
 
 import cellsociety.grid.Grid;
+import cellsociety.cells.FireCell;
 
 public class FireController extends Controller{
 
@@ -8,13 +9,20 @@ public class FireController extends Controller{
   public FireController(Grid grid)  {
     super(grid);
   }
-  @Override
-  public void updateState() {
-
-  }
 
   @Override
   public boolean simulationEnded() {
-    return false;
+    Grid oldGrid = super.getOldGrid();
+    Grid newGrid = super.getNewGrid();
+    int[] dims = oldGrid.getSizeOfGrid();
+
+    for (int i = 0; i < dims[0]; i++) {
+      for (int j = 0; j< dims[1]; j++)  {
+        if (oldGrid.getCellAtLocation(i, j).getState() != newGrid.getCellAtLocation(i, j).getState()) {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
