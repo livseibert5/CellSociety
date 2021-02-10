2611d6e6bdd474fe8ff39968a498011891fcec0b
Billy Luqiu <wyl6@duke.edu>
2021-02-10 04:05:07 -0500

    changed SegregationController to account for moving of cells

--- src/cellsociety/controller/SegregationController.java
+++ src/cellsociety/controller/SegregationController.java
@@ -1,13 +1,59 @@
 package cellsociety.controller;
 
+import cellsociety.cells.Cell;
 import cellsociety.cells.SegregationCell;
 import cellsociety.grid.Grid;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SegregationController extends Controller{
 
-
+  private List<Integer> statesToAddAtCurrentIteration = new ArrayList();
+  private boolean[][] updated;
+  private int[] dims;
   public SegregationController(Grid grid)  {
     super(grid);
+    Grid oldGrid = super.getOldGrid();
+    dims = oldGrid.getSizeOfGrid();
+    updated = new boolean[dims[0]][dims[1]];
+  }
+
+  @Override
+  public void updateState() {
+    Grid oldGrid = super.getOldGrid();
+    Grid newGrid = super.getNewGrid();
+    for (int i = 0; i < dims[0]; i++) {
+      for (int j = 0; j< dims[1]; j++)  {
+        SegregationCell oldCell = (SegregationCell) oldGrid.getCellAtLocation(i,j);
+        int oldState = oldCell.getState();
+        oldCell.determineNextState();
+        oldCell.updateState();
+        int newState = oldCell.getState();
+        if (newState == 3)  {
+          statesToAddAtCurrentIteration.add(oldState);
+        }
+        else  {
+          newGrid.setCellAtLocation(i, j, new SegregationCell(oldState, i, j, null));
+          updated[i][j] = true;
+        }
+      }
+    }
+    addRemovedCells(newGrid);
+  }
+  private void addRemovedCells(Grid newGrid) {
+    Collections.shuffle(statesToAddAtCurrentIteration);
+    for (int i = 0; i < dims[0]; i++) {
+      for (int j = 0; j < dims[1]; j++) {
+        if (statesToAddAtCurrentIteration.size() == 0) return;
+        if (!updated[i][j]) {
+          newGrid.setCellAtLocation(i, j, new SegregationCell(
+              statesToAddAtCurrentIteration.remove(0), i, j, null));
+          updated[i][j] = true;
+        }
+      }
+    }
+
   }
 
   @Override
