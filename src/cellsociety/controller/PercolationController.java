aaf16873ef0b6041e2a68b9b3ac4347c2a02d9a8
Billy Luqiu <wyl6@duke.edu>
2021-02-10 04:13:03 -0500

    added PercolationController

---
+++ src/cellsociety/controller/PercolationController.java
@@ -0,0 +1,10 @@
+package cellsociety.controller;
+
+import cellsociety.grid.Grid;
+
+public class PercolationController extends Controller{
+
+  public PercolationController(Grid oldGrid) {
+    super(oldGrid);
+  }
+}
