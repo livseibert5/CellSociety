b307f13de27145ad90cfebd43d31794bfcce1bff
Billy Luqiu <wyl6@duke.edu>
2021-02-11 04:03:38 -0500

    added default watorcontroller class

---
+++ src/cellsociety/controller/WatorController.java
@@ -0,0 +1,10 @@
+package cellsociety.controller;
+
+import cellsociety.grid.Grid;
+
+public class WatorController extends Controller{
+
+  public WatorController(Grid oldGrid) {
+    super(oldGrid);
+  }
+}
