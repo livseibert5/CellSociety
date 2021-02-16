7e4b8b427e0c3d3cec701ea9cea7a52ef6162e53
Livia <las120@duke.edu>
2021-02-16 18:22:26 -0500

    finished GridToXML and GridFile

--- src/cellsociety/grid/GridToXML.java
+++ src/cellsociety/grid/GridToXML.java
@@ -6,6 +6,8 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -28,12 +30,36 @@ public class GridToXML {
   private Element paramsElement;
   private Map<String, String> simulationData;
   private Controller controller;
+  private String fileName;
 
-  public GridToXML(Controller controller, Map<String, String> simulationData) throws ParserConfigurationException {
+  /**
+   * Constructor for GridToXML class that takes information about the current state of gameplay and
+   * turns it into an XML file.
+   *
+   * @param controller     current controller for simulation
+   * @param simulationData map with data about the simulation
+   * @throws ParserConfigurationException
+   */
+  public GridToXML(Controller controller, Map<String, String> simulationData)
+      throws ParserConfigurationException, TransformerException {
     this.controller = controller;
     this.simulationData = simulationData;
     initializeDocumentBuilder();
     createDocument();
+    exportNewFile();
+  }
+
+  /**
+   * Exports completed XML file so it can be used.
+   *
+   * @throws TransformerException
+   */
+  private void exportNewFile() throws TransformerException {
+    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+    Transformer transformer = transformerFactory.newTransformer();
+    DOMSource source = new DOMSource(doc);
+    StreamResult result = new StreamResult(new File(fileName + ".xml"));
+    transformer.transform(source, result);
   }
 
   private void initializeDocumentBuilder() throws ParserConfigurationException {
@@ -47,18 +73,33 @@ public class GridToXML {
     rootElement = doc.createElement("Simulation");
     doc.appendChild(rootElement);
 
-    for (String str: simulationData.keySet()) {
+    for (String str : simulationData.keySet()) {
       createNewElementWithText(str);
     }
 
     paramsElement = doc.createElement("ConfigParams");
     rootElement.appendChild(paramsElement);
     Map<String, Double> params = controller.getNewGrid().getParams();
-    for (String str: params.keySet()) {
+    for (String str : params.keySet()) {
       createNewParameter(str, params.get(str));
     }
 
     createWidthAndHeight();
+    fileName = java.time.LocalDate.now().toString();
+    createGridFile(fileName);
+  }
+
+  /**
+   * Uses the GridFile class to write a new .txt file with the current grid layout, then creates an
+   * element in the new XML file that references the .txt file.
+   *
+   * @param fileName name for new .txt file
+   */
+  private void createGridFile(String fileName) {
+    Element layout = doc.createElement("LayoutFile");
+    GridFile gridFile = new GridFile(fileName, controller.getNewGrid());
+    layout.appendChild(doc.createTextNode(fileName + ".txt"));
+    rootElement.appendChild(layout);
   }
 
   private void createNewElementWithText(String name) {
@@ -81,9 +122,11 @@ public class GridToXML {
 
   private void createWidthAndHeight() {
     Element width = doc.createElement("Width");
-    width.appendChild(doc.createTextNode(Integer.toString(controller.getNewGrid().getSizeOfGrid()[1])));
+    width.appendChild(
+        doc.createTextNode(Integer.toString(controller.getNewGrid().getSizeOfGrid()[1])));
     Element height = doc.createElement("Height");
-    height.appendChild(doc.createTextNode(Integer.toString(controller.getNewGrid().getSizeOfGrid()[0])));
+    height.appendChild(
+        doc.createTextNode(Integer.toString(controller.getNewGrid().getSizeOfGrid()[0])));
     rootElement.appendChild(width);
     rootElement.appendChild(height);
   }
